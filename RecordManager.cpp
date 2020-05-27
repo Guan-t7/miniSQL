17c441f137cd7e0d1deb5186e178fe2d92835528
Guan <guan1798x@hotmail.com>
2020-05-27 21:59:23 +0800

    一点rm

--- RecordManager.cpp
+++ RecordManager.cpp
@@ -1,12 +1,57 @@
 #include "RecordManager.h"
+#include "pch.h"
 
+using namespace std;
 
-
-RecordManager::RecordManager()
+RecordManager::RecordManager() : bm(BufferManager::instance())
 {
+	
 }
 
 
 RecordManager::~RecordManager()
 {
 }
+
+int RecordManager::create_table(string tableName)
+{
+	ofstream fo(tableName + ".mdbf");
+	return fo.fail();
+}
+
+int RecordManager::drop_table(string tableName)
+{
+	int r = remove((tableName + ".mdbf").c_str());
+	bm.inform_deletion(tableName + ".mdbf");
+	return r;
+}
+
+struct Entry
+{
+	unsigned offs_start;
+	unsigned size;
+};
+struct Sl_Pg_Head
+{
+	unsigned n_entries;
+	unsigned end_fspace;
+	Entry en;
+};
+
+void RecordManager::select_record(string tableName, ...)
+{
+	// sel all below
+	int n_pages; //todo get from Catalog
+	for (size_t i = 0; i < n_pages; i++)
+	{
+		const void* p_pg = bm.getPage_r(make_tuple(tableName + ".mdbf", i));
+		const Sl_Pg_Head* p_slPage = static_cast<const Sl_Pg_Head*> (p_pg);
+		const Entry* _p_entry = &p_slPage->en;
+		for (size_t i = 0; i < p_slPage->n_entries; i++)
+		{
+			const void *p_record = static_cast<const void*>(static_cast<const char*> (p_pg) + _p_entry->offs_start);	//todo return?
+			_p_entry++;
+		}
+	}
+}
+//? UNTESTED
--- RecordManager.h
+++ RecordManager.h
@@ -1,11 +1,22 @@
 #pragma once
+#include "BufferManager.h"
 
-
+using p_Entry = tuple<unsigned, unsigned>;	// pageNum, offs
+// 预计使用 ch10 - Slotted Page Structure
 
 class RecordManager
 {
+private:
+	BufferManager& bm;
+
 public:
 	RecordManager();
 	~RecordManager();
+	// 0 for success below
+	int create_table(string tableName); 
+	int drop_table(string tableName); //? whose duty to validate tableName
+	void select_record(string tableName, ...); //? 条件怎么传，结果怎么返回
+	p_Entry insert_record(string tableName, ...); //? 记录怎么传
+	void delete_record(string tableName, ...);
 };
 
