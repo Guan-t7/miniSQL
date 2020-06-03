66786d3b0f9fc5d51d92bbeeccb96f87d19fbf94
Guan <guan1798x@hotmail.com>
2020-06-03 11:41:18 +0800

    SelRes COND接口: string改成enum；rm接口基本成型

--- RecordManager.h
+++ RecordManager.h
@@ -1,22 +1,81 @@
 #pragma once
 #include "BufferManager.h"
+#include "SimpleSQLInterpreter/DBInfo.h"
+#include "SimpleSQLInterpreter/CatalogManager.h"
+#include "SimpleSQLInterpreter/SelectResult.h"
 
 using p_Entry = tuple<unsigned, unsigned>;	// pageNum, offs
 // 预计使用 ch10 - Slotted Page Structure
+
+// Items within a single record 口区
+// continuous storage; 同一种数据类型 comparable; has a size(probably variable)
+class _DataType
+{
+public:
+	virtual ~_DataType() {}
+	virtual size_t size() const = 0;
+	virtual bool operator<(const _DataType &d2) const = 0;
+	virtual bool operator==(const _DataType &d2) const = 0;
+	virtual string to_string() const = 0;
+	virtual void dump(char* p) const = 0; // 序列化到page上
+};
+// emmm要做的其实是 根据提示进行序列化和反序列化
 
 class RecordManager
 {
 private:
 	BufferManager& bm;
+	CatalogManager cm;
 
+	_DataType * mk_obj(std::pair<DataType, int>& type, const void * mp_record);
+	_DataType * mk_obj(std::pair<DataType, int>& type, const string& val);
+	bool cond_fit(Condition & c, _DataType * data, _DataType * cond_val);
 public:
 	RecordManager();
 	~RecordManager();
-	// 0 for success below
+	// 0 for success
 	int create_table(string tableName); 
 	int drop_table(string tableName); //? whose duty to validate tableName
-	void select_record(string tableName, ...); //? 条件怎么传，结果怎么返回
-	p_Entry insert_record(string tableName, ...); //? 记录怎么传
-	void delete_record(string tableName, ...);
+	vector<vector<string>> select (string tableName, vector<Condition> conds); //? Column是否能保证字段的顺序
+	int insert(string tableName, std::vector<std::string> s_vals); 	//todo I have p_Entry for index insert
+	//! this one returns count of records deleted
+	int delete_rec(string tableName, vector<Condition> conds);
 };
 
+
+template<typename T> // int, double
+class T_Type : public _DataType
+{
+	T d;
+public:
+	T_Type(const T val) : d(val) {}
+	T_Type(const void* p) { d = *(T*)p; }
+	size_t size() const { return sizeof(T); }
+	bool operator<(const _DataType &d2) const { return d < dynamic_cast<const T_Type &>(d2).d; }
+	bool operator==(const _DataType &d2) const { return d == dynamic_cast<const T_Type &>(d2).d; }
+	string to_string() const { return std::to_string(d); }
+	void dump(char* p) const { *(T*)p = d; }
+};
+class _StringType : public _DataType
+{
+	string d;
+	size_t m_size;
+public:
+	_StringType(string val) :d(val), m_size(val.size()) {}
+	_StringType(const void* p, size_t size) :d((char*)p, (char*)p + size), m_size(size) {}
+	size_t size() const { return m_size; }
+	bool operator<(const _DataType &d2) const { return d < dynamic_cast<const _StringType &>(d2).d; }
+	bool operator==(const _DataType &d2) const { return d == dynamic_cast<const _StringType &>(d2).d; }
+	string to_string() const { return d; }
+	void dump(char* p) const {  }
+};
+
+
+
+//todo bad interface
+pair<DataType, int> eType(pair<std::string, int> type)
+{
+	if (type.first == "int") return make_pair(IntType, 0);
+	else if (type.first == "float") return make_pair(FloatType, 0);
+	else return make_pair(StringType, type.second);
+}
--- SimpleSQLInterpreter/SelectResult.h
+++ SimpleSQLInterpreter/SelectResult.h
@@ -7,10 +7,14 @@ class SelectResult :
 public:
 	std::string print() override;
 private:
-	TableInfo tableInfo;
+	TableInfo tableInfo; //? 为什么需要返回这个
 	std::vector<std::vector<std::string>> data;
 };
 
+enum opType { E, NE, L, G, LE, GE }op; // rel_op, ref. x86 asm
 struct Condition {
-	std::string col, val, op;
+	std::string col, val;
+	opType op;
+	
+	Condition(string _col, opType _op, string _val): col(_col),op(_op),val(_val){}
 };
